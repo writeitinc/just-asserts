b0145e10ecc53480763ea47ed8acf86b0b1da527
writeitinc <writeitinc99@gmail.com>
2024-02-29 00:09:52 -0700

    add assumption macros

--- src/just-asserts.h
+++ src/just-asserts.h
@@ -24,6 +24,14 @@
 #define ja_dbg_expect(expr) JA__DEBUG_ONLY(ja_expect(expr))
 #define ja_dbg_expect_msg(expr, msg) JA__DEBUG_ONLY(ja_expect_msg(expr, msg))
 
+#if JA_DEBUG
+# define ja_assume_true(expr) (ja_dbg_expect_msg((expr), "Unmet assumption"), (expr))
+# define ja_assume_false(expr) (ja_dbg_expect_msg(!(expr), "Unmet assumption"), (expr))
+#else
+# define ja_assume_true(expr) (1)
+# define ja_assume_false(expr) (0)
+#endif
+
 void ja__warn(const char *file, unsigned int line, const char *func, const char *fmt, ...);
 void ja__error(const char *file, unsigned int line, const char *func, const char *fmt, ...);
 
