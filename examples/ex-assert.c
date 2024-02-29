700541193dd9babc2bd5f487d8c7c46c8d3f95b4
writeitinc <writeitinc99@gmail.com>
2024-02-28 19:07:50 -0700

    add ex-assert

---
+++ examples/ex-assert.c
@@ -0,0 +1,21 @@
+#define JA_IMPLEMENTATION
+#include <just-asserts/just-asserts.h>
+
+int main(void)
+{
+	ja_assert(1);
+	ja_assert_msg(sizeof(long) == sizeof(long int),
+			"`long` and `long int` should be the same type");
+
+	// ja_assert(0); // <-- runtime error (aborts)
+	
+	ja_expect(1);
+
+	// one of these will (probably) cause a runtime warning
+	ja_expect_msg(sizeof(long) == sizeof(long long),
+			"Expected `long` and `long long` to be the same size");
+	ja_expect_msg(sizeof(long) == sizeof(int),
+			"Expected `long` and `int` to be the same size");
+
+	return 0;
+}
