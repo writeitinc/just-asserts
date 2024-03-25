3f4f88f09bb465839d1f9b31107001c7b3350164
writeitinc <writeitinc99@gmail.com>
2024-03-24 22:28:23 -0600

    construct runtime assert macros more composably

--- src/ja-runtime-asserts.h
+++ src/ja-runtime-asserts.h
@@ -1,6 +1,9 @@
 #ifndef ja_runtime_asserts_h
 #define ja_runtime_asserts_h
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #if !defined(JA_DEBUG)
 # define JA_DEBUG 0
 #endif
@@ -11,17 +14,25 @@
 # define JA__DEBUG_ONLY(expr) (void)0
 #endif
 
-#define JA__WARN(fmt, ...) ja__warn(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
-#define JA__ERROR(fmt, ...) ja__error(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
-
 #define ja_assert(expr) ja_assert_msg(expr, "Failed assertion")
 #define ja_expect(expr) ja_expect_msg(expr, "Unmet expectation")
-#define ja_assert_msg(expr, msg) ((expr) \
+#define ja_assert_msg(expr, ...) ((expr) \
 		? (void)0 \
-		: JA__ERROR("%s: `%s`", msg, #expr))
-#define ja_expect_msg(expr, msg) ((expr) \
+		: (void)( \
+			ja__report_trace("err"), \
+			ja__report_msg(__VA_ARGS__), \
+			ja__report_char('\n'), \
+			exit(EXIT_FAILURE), \
+			(void)0 \
+		))
+#define ja_expect_msg(expr, ...) ((expr) \
 		? (void)0 \
-		: JA__WARN("%s: `%s`", msg, #expr))
+		: (void)( \
+			ja__report_trace("warn"), \
+			ja__report_msg(__VA_ARGS__), \
+			ja__report_char('\n'), \
+			(void)0 \
+		))
 
 #define ja_dbg_assert(expr) JA__DEBUG_ONLY(ja_assert(expr))
 #define ja_dbg_assert_msg(expr, msg) JA__DEBUG_ONLY(ja_assert_msg(expr, msg))
@@ -36,49 +47,13 @@
 # define ja_assume_false(expr) (0)
 #endif
 
-void ja__warn(const char *file, unsigned int line, const char *func, const char *fmt, ...);
-void ja__error(const char *file, unsigned int line, const char *func, const char *fmt, ...);
-
-#ifdef JA_IMPLEMENTATION
-
-# include <stdarg.h>
-# include <stdio.h>
-# include <stdlib.h>
-
-static void ja__report(const char *report_type, const char *file, unsigned int line,
-		const char *func, const char *fmt, va_list va_args);
-
-void ja__warn(const char *file, unsigned int line, const char *func, const char *fmt, ...)
-{
-	va_list va_args;
-	va_start(va_args, fmt);
-
-	ja__report("warn", file, line, func, fmt, va_args);
-
-	va_end(va_args);
-}
-
-void ja__error(const char *file, unsigned int line, const char *func, const char *fmt, ...)
-{
-	va_list va_args;
-	va_start(va_args, fmt);
-
-	ja__report("err", file, line, func, fmt, va_args);
-
-	va_end(va_args);
-
-	exit(EXIT_FAILURE);
-}
-
-void ja__report(const char *report_type, const char *file, unsigned int line, const char *func,
-		const char *fmt, va_list va_args)
-{
-	fprintf(stderr, "[ja:%s] <%s:%s:%u>\n", report_type, file, func, line);
-	fputc('\t', stderr);
-	vfprintf(stderr, fmt, va_args);
-	fputc('\n', stderr);
-}
-
-#endif // JA_IMPLEMENTATION
+#define ja__report(...) fprintf(stderr, __VA_ARGS__)
+#define ja__report_char(c) putc((c), stderr)
+#define ja__report_trace(severity) \
+	ja__report("[ja:%s] <%s:%s:%u>\n", severity, __FILE__, __func__, __LINE__)
+#define ja__report_msg(...) ( \
+		ja__report_char('\t'), \
+		ja__report(__VA_ARGS__), \
+		ja__report_char('\n'))
 
 #endif // ja_runtime_asserts_h
