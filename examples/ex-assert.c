b4e52f94fec6baa6f608780f8cd0022a4e8f369e
writeitinc <writeitinc99@gmail.com>
2024-04-04 02:06:45 -0600

    add Vec2D example to ex-assert

--- examples/ex-assert.c
+++ examples/ex-assert.c
@@ -1,6 +1,16 @@
 #define JA_IMPLEMENTATION
 #include <just-asserts/ja-runtime-asserts.h>
 
+typedef struct Vec2D {
+	float x;
+	float y;
+} Vec2D;
+
+#define JA_TYPE_Vec2D Vec2D
+#define JA_EQUALS_Vec2D(a, b) ((a).x == (b).x && (a).y == (b).y)
+#define JA_FMT_Vec2D "[%g, %g]"
+#define JA_FMT_ARGS_Vec2D(v) (v).x, (v).y
+
 int main(void)
 {
 	ja_assert(1);
@@ -23,6 +33,10 @@ int main(void)
 
 	ja_assert_neq(float, 1.0f, 1.5f);
 	ja_expect_neq(int, 1.0f, 1.5f); // runtime warning
+
+	Vec2D vec = { 2, 1.5 };
+	ja_expect_eq(Vec2D, vec, ((Vec2D){ 2, 1.5 })); // compound literals require parentheses
+	ja_expect_eq(Vec2D, ((Vec2D){ 1, 2 }), ((Vec2D){ 2, 1 })); // runtime warning
 
 	return 0;
 }
